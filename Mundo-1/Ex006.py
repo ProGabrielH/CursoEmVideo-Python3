892e30d59d26dc87b6d808d43b31d2352c3ce3e1
Gabriel Henrique <gabrielh.jrf@gmail.com>
2023-08-27 01:17:10 -0300

    add exercises

---
+++ Mundo-1/Ex006.py
@@ -0,0 +1,3 @@
+# Crie um algoritimo que faça o dobro, triplo e raiz quadrada de um numero
+num = int(input('Digite um valor: '))
+print(f'O dobro desse número é: {num*2}! \nO triplo é: {num*3}! \nE sua raiz quadrada é: {num**1/2:.1f}!')
